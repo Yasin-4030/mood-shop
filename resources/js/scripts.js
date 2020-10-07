c0d6e764fdd6d3390bfb9a00d7934c54697ee627
Mohammad Yasin <mohammad.yasin@students.makeschool.com>
2020-10-07 11:36:26 -0700

    displayed moods with button using javascript

---
+++ resources/js/scripts.js
@@ -0,0 +1,30 @@
+const itemsContainer = document.getElementById('items') 
+import data from './data.js'
+
+for (let i=0; i<data.length; ++i) {
+    let newDiv = document.createElement('div');
+    newDiv.className = 'item'
+
+    let img = document.createElement('img');
+    console.log(data[i])
+    img.src = data[i].image
+    img.width = 300
+    img.height = 300
+    newDiv.appendChild(img)
+    
+    let desc = document.createElement('P')
+    desc.innerText = data[i].desc
+    newDiv.appendChild(desc)
+    let price = document.createElement('P')
+    price.innerText = data[i].price
+    newDiv.appendChild(price)
+
+    let button = document.createElement('button');
+    button.id = data[i].name
+
+    button.dataset.price = data[i].price
+    button.innerHTML = "Add to Cart"
+    newDiv.appendChild(button)
+
+    itemsContainer.appendChild(newDiv)
+}
